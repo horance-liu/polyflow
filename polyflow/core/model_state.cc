842358cba2bd90d7c3ad9b8f3a4a3948c8dd167e
horance <horance@outlook.com>
2019-12-12 07:16:55 +0800

    process return value for ModelLoader::loadModel/unloadModel

--- polyflow/core/model_state.cc
+++ polyflow/core/model_state.cc
@@ -14,14 +14,14 @@ inline cub::Status ModelState::transfer(State from, State to) {
 
 cub::Status ModelState::onLoad(ModelLoader& loader) {
   CUB_ASSERT_SUCC_CALL(transfer(NEW, LOADING));
-  loader.loadModel();
+  CUB_ASSERT_SUCC_CALL(loader.loadModel());
   CUB_ASSERT_SUCC_CALL(transfer(LOADING, READY));
   return cub::Success;
 }
 
 cub::Status ModelState::onUnload(ModelLoader& loader) {
   CUB_ASSERT_SUCC_CALL(transfer(READY, UNLOADING));
-  loader.unloadModel();
+  CUB_ASSERT_SUCC_CALL(loader.unloadModel());
   CUB_ASSERT_SUCC_CALL(transfer(UNLOADING, DISABLED));
   return cub::Success;
 }
